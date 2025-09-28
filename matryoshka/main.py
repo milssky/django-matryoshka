2c7eb5898fd3e5043ff78708bb9236ec61ad59ce
Alexey Potapov <2furius@gmail.com>
2025-09-28 14:01:59 +0300

    chore(deps): add dev tools, refs #1

---
+++ matryoshka/main.py
@@ -0,0 +1,18 @@
+# TODO: Boilerplate
+class MatryoshkaAPI:
+    """
+    ``Matryoshka API`` app class.
+
+    The main entry point to use MatryshkaAPI.
+
+    ## Example
+
+    ```python
+    from matryoshka import MatryoshkaAPI
+
+    api = MatryoshkaAPI()
+    ```
+    """
+
+    def __init__(self) -> None:
+        return None
